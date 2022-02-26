2ef8615a145ba75daebee20dc0f282344d3ed038
OXS090 <olivershooter@gmail.com>
2022-02-26 16:13:28 +0000

    V0.0.1

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import bg from "./assets/bg.jpeg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        source={bg}
+        style={styles.backgroundImage}
+        resizeMode="contain"
+      >
+        <View style={styles.nought} />
+
+        <View>
+          <View style={styles.crossLine} />
+          <View style={(styles.crossLine, styles.crossLineInvert)} />
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#242D34",
+  },
+
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+
+    //to force the nought into the center
+    paddingTop: 15,
+  },
+
+  nought: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+
+    borderWidth: 10,
+    borderColor: "white",
+  },
+
+  crossLine: {
+    position: "absolute",
+    width: 10,
+    height: 70,
+    borderRadius: 5,
+    backgroundColor: "white",
+    transform: [{ rotate: "45deg" }],
+  },
+
+  crossLineInvert: {
+    position: "absolute",
+    width: 10,
+    height: 70,
+    borderRadius: 5,
+    backgroundColor: "white",
+    transform: [{ rotate: "-45deg" }],
   },
 });
