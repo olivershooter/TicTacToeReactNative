b7b14150b17b0bf7d74a882f31e5b12ab7bb0c8e
OXS090 <olivershooter@gmail.com>
2022-02-27 16:02:31 +0000

    Check winning state implemented

--- App.js
+++ App.js
@@ -1,8 +1,83 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Pressable,
+  Alert,
+} from "react-native";
 import bg from "./assets/bg.jpeg";
 
+import React, { useState } from "react";
+
 export default function App() {
+  const [boardMap, setMap] = useState([
+    ["", "", ""], //1st Row
+    ["", "", ""], //2nd Row
+    ["", "", ""], //3rd Row
+  ]);
+
+  const [currentTurn, setCurrentTurn] = useState("x");
+
+  const onPress = (rowIndex, columnIndex) => {
+    console.warn("hello", rowIndex, columnIndex);
+
+    if (boardMap[rowIndex][columnIndex] !== "") {
+      Alert.alert("Position already occupied");
+      return;
+    }
+
+    setMap((existingMap) => {
+      const updatedMap = [...existingMap];
+      updatedMap[rowIndex][columnIndex] = currentTurn;
+      return updatedMap;
+    });
+
+    setCurrentTurn(currentTurn === "x" ? "o" : "x");
+
+    checkWinningState();
+  };
+
+  const checkWinningState = () => {
+    //Check the rows
+    for (let i = 0; i < 3; i++) {
+      const isTheRowAllX = boardMap[i].every((cell) => cell === "x");
+      const isTheRowAllO = boardMap[i].every((cell) => cell === "o");
+      if (isTheRowAllX) {
+        Alert.alert(`X's won. Row: ${i}.`);
+      }
+
+      if (isTheRowAllO) {
+        Alert.alert(`O's won. Row: ${i}.`);
+      }
+    }
+
+    //Check the columns
+    for (let col = 0; col < 3; col++) {
+      let isTheColumnAllX = true;
+      let isTheColumnAllO = true;
+
+      for (let row = 0; row < 3; row++) {
+        if (boardMap[row][col] != "x") {
+          isTheColumnAllX = false;
+        }
+        if (boardMap[row][col] != "o") {
+          isTheColumnAllO = false;
+        }
+      }
+
+      if (isTheColumnAllX) {
+        Alert.alert(`X's won. Column: ${col}.`);
+      }
+
+      if (isTheColumnAllO) {
+        Alert.alert(`O's won. Column: ${col}.`);
+      }
+    }
+    //Check the diagonals
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -10,11 +85,35 @@ export default function App() {
         style={styles.backgroundImage}
         resizeMode="contain"
       >
-        <View style={styles.nought} />
+        <View style={styles.map}>
+          {boardMap.map((row, rowIndex) => (
+            <View key={`row-${rowIndex}`} style={styles.mapRow}>
+              {row.map((cell, columnIndex) => (
+                <Pressable
+                  key={`row-${rowIndex}-${columnIndex}`}
+                  onPress={() => onPress(rowIndex, columnIndex)}
+                  style={styles.mapCell}
+                >
+                  {cell === "o" && <View style={styles.nought} />}
+                  {cell === "x" && (
+                    <View style={styles.crossContainer}>
+                      <View style={styles.crossLine} />
+                      <View
+                        style={[styles.crossLine, styles.crossLineInvert]}
+                      />
+                    </View>
+                  )}
+                </Pressable>
+              ))}
+            </View>
+          ))}
 
-        <View>
-          <View style={styles.crossLine} />
-          <View style={(styles.crossLine, styles.crossLineInvert)} />
+          {/* <View style={styles.nought} />
+
+          <View style={styles.crossContainer}>
+            <View style={styles.crossLine} />
+            <View style={[styles.crossLine, styles.crossLineInvert]} />
+          </View> */}
         </View>
       </ImageBackground>
       <StatusBar style="auto" />
@@ -41,9 +140,20 @@ const styles = StyleSheet.create({
     paddingTop: 15,
   },
 
+  map: {
+    width: "80%",
+    aspectRatio: 1,
+  },
+
+  mapCell: { flex: 1 },
+
+  mapRow: {
+    flex: 1,
+    flexDirection: "row",
+  },
+
   nought: {
-    width: 75,
-    height: 75,
+    flex: 1,
     borderRadius: 50,
     alignItems: "center",
     justifyContent: "center",
@@ -53,21 +163,21 @@ const styles = StyleSheet.create({
     borderColor: "white",
   },
 
+  crossContainer: {
+    flex: 1,
+  },
+
   crossLine: {
     position: "absolute",
+    left: "48%",
     width: 10,
-    height: 70,
+    height: "100%",
     borderRadius: 5,
     backgroundColor: "white",
     transform: [{ rotate: "45deg" }],
   },
 
   crossLineInvert: {
-    position: "absolute",
-    width: 10,
-    height: 70,
-    borderRadius: 5,
-    backgroundColor: "white",
     transform: [{ rotate: "-45deg" }],
   },
 });
